1098e4cc3459659e9c34b90dad44efc54ae640e3
hana <dlagksk64@naver.com>
2022-11-12 11:28:08 +0900

    style: global style format

---
+++ src/styles/global.ts
@@ -0,0 +1,76 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import NotoSansKRBoldOTF from "../assets/NotoSansKR-Black.otf";
+import NotoSansKRBoldWOFF from "../assets/font/NotoSansKR-Bold.woff";
+import NotoSansKRBoldWOFF2 from "../assets/font/NotoSansKR-Bold.woff2";
+import NotoSansKRLightOTF from "../assets/font/NotoSansKR-Light.otf";
+import NotoSansKRLightWOFF from "../assets/font/NotoSansKR-Light.woff";
+import NotoSansKRLightWOFF2 from "../assets/font/NotoSansKR-Light.woff2";
+import NotoSansKRRegularOTF from "../assets/font/NotoSansKR-Regular.otf";
+import NotoSansKRRegularWOFF from "../assets/font/NotoSansKR-Regular.woff";
+import NotoSansKRRegularWOFF2 from "../assets/font/NotoSansKR-Regular.woff2";
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+  html,
+  body{
+    background: #0C0F0F;
+     ::-webkit-scrollbar {
+    display: none;
+  }
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+  }
+  a {
+        text-decoration: none;
+        color: inherit;
+    }
+  * {
+      box-sizing: border-box;
+  }
+  input, textarea { 
+    -moz-user-select: auto;
+    -webkit-user-select: auto;
+    -ms-user-select: auto;
+    user-select: auto;
+  }
+  input:focus {
+    outline: none;
+  }
+
+  button {
+    border: none;
+    background: none;
+    padding: 0;
+    cursor: pointer;
+  }
+  @font-face {
+    font-family: 'NotoSansKRBold';
+    src: url(${NotoSansKRBoldWOFF2}) format('woff2'),
+    url(${NotoSansKRBoldWOFF}) format('woff'),
+    url(${NotoSansKRBoldOTF}) format('otf'),
+  }
+  @font-face {
+    font-family: 'NotoSansKRLight';
+    src: url(${NotoSansKRLightWOFF2}) format('woff2'),
+    url(${NotoSansKRLightWOFF}) format('woff'),
+    url(${NotoSansKRLightOTF}) format('otf'),
+  }
+  @font-face {
+    font-family: 'NotoSansKRRegular';
+    src: url(${NotoSansKRRegularWOFF2}) format('woff2'),
+    url(${NotoSansKRRegularWOFF}) format('woff'),
+    url(${NotoSansKRRegularOTF}) format('otf'),
+  }
+
+  * {
+    box-sizing: border-box;
+    font-family: 'NotoSansKRRegular';
+  }
+
+  a, a:hover {
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyle;
