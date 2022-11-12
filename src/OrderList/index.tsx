783448213e9d0cf85f07d05bcf831dba294bd2e1
hana <dlagksk64@naver.com>
2022-11-12 17:18:16 +0900

    feat: order list setting modal

--- src/OrderList/index.tsx
+++ src/OrderList/index.tsx
@@ -7,6 +7,16 @@ import OrderCard from "./OrderCard";
 const OrderList = () => {
   const [data, setList] = useState<Order[]>([]);
 
+  const [open, setOpen] = useState(false);
+  const [close, setClose] = useState(false);
+
+  const openModal = () => {
+    setOpen(true);
+  };
+  const closeModal = () => {
+    setClose(true);
+    setOpen(false);
+  };
   useEffect(() => {
     selectOrderList().then((res) => setList(res));
   }, []);
@@ -19,8 +29,40 @@ const OrderList = () => {
     });
   };
 
-  return <List>{getList(data)}</List>;
+  return (
+    <List>
+      <div
+        className="order-btn"
+        onClick={() => {
+          openModal();
+        }}
+      >
+        <span>주문하기</span>
+      </div>
+      {getList(data)}
+    </List>
+  );
 };
 
-const List = styled.div``;
+const List = styled.div`
+  display: flex;
+  flex-direction: column;
+  .order-btn {
+    align-self: end;
+    margin: 16px 0;
+    width: 100vw;
+    max-width: 375px;
+    height: 40px;
+    border: 1px solid #868e9c;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+
+    > span {
+      color: #868e9c;
+    }
+  }
+`;
 export default OrderList;
