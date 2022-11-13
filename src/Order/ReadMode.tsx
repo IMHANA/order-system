df57c83e5998bde78a08d4f171ee1e48cb6d9663
hana <dlagksk64@naver.com>
2022-11-13 19:29:43 +0900

    feat: read mode component

---
+++ src/Order/ReadMode.tsx
@@ -0,0 +1,40 @@
+import { IOrder, IUser } from "src/apis/types";
+
+interface Props {
+  userData: IUser;
+  orderData: IOrder;
+}
+const ReadMode = ({ userData, orderData }: Props) => {
+  return (
+    <>
+      <p>주문자 정보</p>
+      <div className="user">
+        <span>주문자ID</span>
+        <span>주문자명</span>
+        <span>주문자 생성일자</span>
+      </div>
+      <div className="user-info">
+        <span>{userData?.id}</span>
+        <span>{userData?.name}</span>
+        <span>{userData?.createdAt}</span>
+      </div>
+
+      <p>주문 정보</p>
+      <div className="order">
+        <span>주문ID</span>
+        <span>배송지 주소1</span>
+        <span>배송지 주소2</span>
+        <span>주문금액</span>
+        <span>주문일시</span>
+      </div>
+      <div className="order-info">
+        <span>{orderData?.id}</span>
+        <span>{orderData?.address1}</span>
+        <span>{orderData?.address2}</span>
+        <span>{orderData?.totalPrice}</span>
+        <span>{orderData?.createdAt}</span>
+      </div>
+    </>
+  );
+};
+export default ReadMode;
